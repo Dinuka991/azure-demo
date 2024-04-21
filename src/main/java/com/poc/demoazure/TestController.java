faf595f111169b2309fbb768462e8fc7295b4281
Dinuka Ekanayake <dinukaekanayake92@gmail.com>
2024-04-21 21:05:29 +0800

    1st commit

--- src/main/java/com/poc/demoazure/TestController.java
+++ src/main/java/com/poc/demoazure/TestController.java
@@ -1,2 +1,12 @@
-package com.poc.demoazure;public class TestController {
+package com.poc.demoazure;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/example")
+    public String getExample() {
+        return "This is an example GET request hello";
+    }
 }
